04d4d45efddaae0ffff54cbbaf8251f7e55a5249
muxau <muxau@LAPTOP-03TISFLE>
2022-10-21 22:37:20 +0200

    Добавьте файлы проекта.

---
+++ Study3/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Study3.Models;
+using System.Diagnostics;
+
+namespace Study3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult HelloWorld()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            string customVar = _configuration.GetValue<string>("CustomVar");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
